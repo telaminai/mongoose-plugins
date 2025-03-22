e37fc81d8f6c241f1baad65806ec0c969395ae87
greg <gregphiggins@gmail.com>
2025-03-22 19:07:47 +0000

    update gitignore. Fix key logging problem

--- service/svc-cache/src/main/java/com/fluxtion/dataflow/serverplugin/svc/cache/JsonFileCache.java
+++ service/svc-cache/src/main/java/com/fluxtion/dataflow/serverplugin/svc/cache/JsonFileCache.java
@@ -124,7 +124,11 @@ public class JsonFileCache implements Cache, Agent, Lifecycle, EventFlowService 
         if (updated.get()) {
             mapper.writerWithDefaultPrettyPrinter()
                     .writeValue(new File(fileName), cacheMap);
-            log.info("cache updated:{} keys:{}", fileName, cacheMap.keySet().stream().mapToLong(Long::parseLong).sorted().toArray());
+            if (cacheMap.isEmpty()) {
+                log.info("cache updated:{} now empty ", fileName);
+            } else {
+                log.info("cache updated:{} keys:{}", fileName, cacheMap.keySet());
+            }
         }
         updated.set(false);
         return 0;
